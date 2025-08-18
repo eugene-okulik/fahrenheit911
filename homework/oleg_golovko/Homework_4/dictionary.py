57c264a0d29501270af031c79bd8a1508dba23a9
Eugeny Okulik <98970620+eugene-okulik@users.noreply.github.com>
2025-08-18 14:43:55 +0200

    fix formatting

--- homework/oleg_golovko/Homework_4/dictionary.py
+++ homework/oleg_golovko/Homework_4/dictionary.py
@@ -1,4 +1,4 @@
-my_dict = { 
+my_dict = {
     'tuple': (1, 3, 6, 7, None, 'text', False, 2.42),
     'list': [1, 3, 6, 7, None, 'text', False, 2.42, 'sdsdf', 'last', 'last2'],
     'dict': {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5},
@@ -13,5 +13,3 @@ my_dict['dict'].pop('a')
 my_dict['set'].add('python')
 my_dict['set'].remove(1)
 print(my_dict)
-
-

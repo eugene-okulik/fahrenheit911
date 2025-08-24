87c8d7947d46c8393aefd6f1a5dad88c0375b9bc
OlegGolovko <oleg.golovko.job@gmail.com>
2025-08-24 16:10:33 +0200

    refactor code

--- homework/oleg_golovko/Homework_6/fuzzbuzz.py
+++ homework/oleg_golovko/Homework_6/fuzzbuzz.py
@@ -3,15 +3,10 @@ fuzz = 'fuzz'
 buzz = 'buzz'
 
 for number in row_numbers:
-    fuzz_buzz = []
     if number % 15 == 0:
-        number = str(number)
-        number = number.replace(number, fuzz.title() + buzz.title())
+        number = fuzz.title() + buzz.title()
     elif number % 3 == 0:
-        number = str(number)
-        number = number.replace(number, fuzz)
+        number = fuzz
     elif number % 5 == 0:
-        number = str(number)
-        number = number.replace(number, buzz)
-    fuzz_buzz.append(number)
-    print(fuzz_buzz[0])
+        number = buzz
+    print(number)
